a0ce6936ee6963587c60a5917fe80efc4bcf46b3
Kwinten Li <kwinten.yc.li@gmail.com>
2020-08-20 18:29:54 +0800

    init approach 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
-const path = require('path');
-const slsw = require('serverless-webpack');
+const path = require("path");
+const slsw = require("serverless-webpack");
 
 module.exports = {
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
   entry: slsw.lib.entries,
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
   },
   output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
+    libraryTarget: "commonjs",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js",
   },
-  target: 'node',
+  target: "node",
+  externals: ["aws-sdk"], // this tells webpack not to bundle 'aws-sdk', we don't need this because AWS lambda environment has this included
   module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-    ],
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
   },
 };
