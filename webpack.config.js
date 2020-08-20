b8ac4fca599371ff160ad6e95ea59abf90872f1c
Kwinten Li <kwinten.yc.li@gmail.com>
2020-08-20 18:55:00 +0800

    revert changes on master

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const slsw = require("serverless-webpack");
 module.exports = {
   mode: slsw.lib.webpack.isLocal ? "development" : "production",
   entry: slsw.lib.entries,
-  devtool: "source-map",
   resolve: {
     extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
   },
@@ -14,7 +13,6 @@ module.exports = {
     filename: "[name].js",
   },
   target: "node",
-  externals: ["uuid", "aws-sdk"], // exclude the dependencies that you want serverless-webpack to upload in the nodule_modules folder
   module: {
     rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
   },
